6db12e1b56da3e717a0ce6de2e0d128cdce3ed04
Erik Stevenson <eriknstevenson@gmail.com>
2016-08-04 08:49:50 -0400

    about maps, fixed mistake in lists

--- src/test/scala/org/functionalkoans/forscala/AboutLists.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutLists.scala
@@ -21,13 +21,13 @@ class AboutLists extends KoanSuite {
     val b: List[Int] = Nil
 
     (a == Nil) should be(true)
-    (a eq Nil) should be(false)
+    (a eq Nil) should be(true)
 
     (b == Nil) should be(true)
-    (b eq Nil) should be(false)
+    (b eq Nil) should be(true)
 
     (a == b) should be(true)
-    (a eq b) should be(false)
+    (a eq b) should be(true)
   }
 
   koan("Lists are easily created") {
